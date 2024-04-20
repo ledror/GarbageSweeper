4fa170a7086b90f19c6e1f086b56646e54d9d732
Dror Elmakias <drorelmakias@gmail.com>
2024-04-20 18:01:05 +0300

    added guild-specific semantics, added !list, !count

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 	"os"
 	"os/signal"
 	"os/user"
 	"path/filepath"
+	"slices"
 	"strings"
 	"syscall"
 
@@ -17,9 +19,22 @@ const (
 	owner         = "YOUR-DISCORD-ID"
 )
 
+type GuildUser struct {
+	guildID string
+	userID  string
+}
+
+type (
+	GuildMap       map[string]bool // set of users
+	GuildCountMap  map[string]int
+	AbusedMap      map[string]GuildMap // map from guildID to set of users
+	AbusedCountMap map[string]GuildCountMap
+)
+
 var (
-	token  string
-	abused map[string]bool
+	token       string
+	abused      AbusedMap
+	abusedCount AbusedCountMap
 )
 
 func init() {
@@ -37,7 +52,8 @@ func init() {
 	}
 	token = string(tokenContent)
 	token = strings.TrimSuffix(token, "\n")
-	abused = make(map[string]bool)
+	abused = make(AbusedMap)
+	abusedCount = make(AbusedCountMap)
 }
 
 func main() {
@@ -79,13 +95,23 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// fmt.Println(m.Content)
 
 	if strings.HasPrefix(m.Content, "!abuse") {
-		addAbuse(s, m)
+		user := identifyUserInCommand(m)
+		if user != "" {
+			addAbuse(s, user, m.GuildID, m.ChannelID)
+		}
 	} else if strings.HasPrefix(m.Content, "!pardon") {
-		addPardon(s, m)
+		user := identifyUserInCommand(m)
+		if user != "" {
+			addPardon(s, user, m.GuildID, m.ChannelID)
+		}
+	} else if strings.HasPrefix(m.Content, "!list") {
+		listAbused(s, m.GuildID, m.ChannelID)
+	} else if strings.HasPrefix(m.Content, "!count") {
+		countAbused(s, m.GuildID, m.ChannelID)
 	}
 }
 
-func addAbuse(s *discordgo.Session, m *discordgo.MessageCreate) {
+func identifyUserInCommand(m *discordgo.MessageCreate) (user string) {
 	statement := strings.Split(m.Content, " ")
 	if len(statement) != 2 {
 		return
@@ -94,39 +120,127 @@ func addAbuse(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if !strings.HasPrefix(target, "<@") || !strings.HasSuffix(target, ">") {
 		return
 	}
-	target = target[2 : len(target)-1]
-	abused[target] = true
-	err := s.GuildMemberMove(m.GuildID, target, nil)
+	user = target[2 : len(target)-1]
+	return
+}
+
+func addAbuse(s *discordgo.Session, userID string, guildID string, channelID string) {
+	if abused[guildID] == nil {
+		abused[guildID] = make(GuildMap)
+	}
+	if _, ok := abused[guildID][userID]; ok {
+		message := fmt.Sprintf("already abusing <@%s>, chillax", userID)
+		s.ChannelMessageSend(channelID, message)
+		fmt.Printf("Already abusing user %s on %s\n", userID, guildID)
+		return
+	}
+	abused[guildID][userID] = true
+	fmt.Printf("Abusing %s in %s\n", userID, guildID)
+	message := fmt.Sprintf("abusing <@%s>, enjoy :)", userID)
+	s.ChannelMessageSend(channelID, message)
+	disconnectUser(s, guildID, userID)
+}
+
+func addPardon(s *discordgo.Session, userID string, guildID string, channelID string) {
+	notFound := false
+	if abused[guildID] == nil {
+		notFound = true
+	} else if _, ok := abused[guildID][userID]; !ok {
+		notFound = true
+	}
+	if notFound {
+		message := fmt.Sprintf("<@%s> isn't being abused dummy", userID)
+		s.ChannelMessageSend(channelID, message)
+		fmt.Printf("%s wasn't abused\n", userID)
+		return
+	}
+	delete(abused[guildID], userID)
+	if len(abused[guildID]) == 0 {
+		delete(abused, guildID)
+	}
+	message := fmt.Sprintf("pardoning <@%s>, stop being a jerk", userID)
+	s.ChannelMessageSend(channelID, message)
+	fmt.Printf("Pardoning %s in %s\n", userID, guildID)
+}
+
+func listAbused(s *discordgo.Session, guildID string, channelID string) {
+	fmt.Println("Listing abused in guild", guildID)
+	if abused[guildID] == nil || len(abused[guildID]) == 0 {
+		message := fmt.Sprintf("no one is being abused")
+		s.ChannelMessageSend(channelID, message)
+		return
+	}
+	message := "currently abusing:"
+	i := 1
+	for user := range abused[guildID] {
+		message += fmt.Sprintf("\n%d. <@%s>", i, user)
+		i++
+	}
+	s.ChannelMessageSend(channelID, message)
+}
+
+type UserCount struct {
+	user  string
+	count int
+}
+
+func countAbused(s *discordgo.Session, guildID string, channelID string) {
+	fmt.Println("counting abused in", guildID)
+	if abusedCount[guildID] == nil || len(abusedCount[guildID]) == 0 {
+		message := "no one was abused, stop crying"
+		s.ChannelMessageSend(channelID, message)
+		return
+	}
+	message := "abusement counts:"
+	var userCounts []UserCount
+
+	for user, count := range abusedCount[guildID] {
+		userCounts = append(userCounts, UserCount{user, count})
+	}
+
+	slices.SortFunc(userCounts, func(a, b UserCount) int {
+		return cmp.Compare(a.count, b.count)
+	})
+
+	for rank, userCount := range userCounts {
+		message += fmt.Sprintf("\n%d. <@%s> -- %d", rank, userCount.user, userCount.count)
+	}
+	s.ChannelMessageSend(channelID, message)
+}
+
+func disconnectUser(s *discordgo.Session, guildID string, userID string) {
+	guild, err := s.State.Guild(guildID)
 	if err != nil {
-		fmt.Printf("Couldn't disconnect user %s: %v\n", target, err)
+		fmt.Printf("Couldn't find guildID %s", guildID)
 		return
 	}
-	fmt.Println("Abusing", target)
-}
-
-func addPardon(s *discordgo.Session, m *discordgo.MessageCreate) {
-	statement := strings.Split(m.Content, " ")
-	if len(statement) != 2 {
+	found := false
+	for _, key := range guild.VoiceStates {
+		if userID == key.UserID {
+			found = true
+			break
+		}
+	}
+	if !found {
+		fmt.Printf("Couldn't find user %s in %s\n", userID, guildID)
 		return
 	}
-	target := strings.Trim(statement[1], " ")
-	if !strings.HasPrefix(target, "<@") || !strings.HasSuffix(target, ">") {
+	err = s.GuildMemberMove(guildID, userID, nil)
+	if err != nil {
+		fmt.Printf("Couldn't disconnect user %s on %s: %v\n", userID, guildID, err)
 		return
 	}
-	target = target[2 : len(target)-1]
-	delete(abused, target)
-	fmt.Println("Pardoning", target)
+	if abusedCount[guildID] == nil {
+		abusedCount[guildID] = make(GuildCountMap)
+	}
+	abusedCount[guildID][userID]++
 }
 
 func voiceStateUpdate(s *discordgo.Session, event *discordgo.VoiceStateUpdate) {
-	_, ok := abused[event.UserID]
-	if !ok {
+	if event.BeforeUpdate != nil { // this isn't a "user connected" event
 		return
 	}
-
-	err := s.GuildMemberMove(event.GuildID, event.UserID, nil)
-	if err != nil {
-		fmt.Printf("Couldn't disconnect user %s: %v\n", event.UserID, err)
-		return
+	if _, ok := abused[event.GuildID][event.UserID]; ok {
+		disconnectUser(s, event.GuildID, event.UserID)
 	}
 }
